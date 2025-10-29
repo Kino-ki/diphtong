c2ac1b054572a2f307b1dafb506db53d99c89501
faiza fehri <faiza.fehri+github@gmail.com>
2025-10-29 03:55:10 -0400

    fix padding

--- components/sectionshome/SectionCta.tsx
+++ components/sectionshome/SectionCta.tsx
@@ -20,7 +20,7 @@ export default function SectionCta() {
         <div className="flex flex-col lg:flex-row justify-between mr-[0.2rem] h-full  px-5 lg:px-0">
           <div data-speed="" className="flex flex-col lg:w-1/3 w-4/5 ">
             <div className="lg:h-[30svh] "></div>
-            <div className="bg-wlite relative  w-full flex flex-col justify-center lg:h-full h-[35svh] mt-32 ">
+            <div className="bg-wlite relative  w-full flex flex-col justify-center lg:h-full min-h-[35svh]  mt-32 ">
               <p className="text-diphblack px-[2rem] text-xl md:text-2xl md:leading-[3rem] 2xl:text-3xl text-start 2xl:leading-[4.6rem] tracking-wider ">
                 {home.ctaleft.map((part, i) =>
                   typeof part === "string" ? part : <b key={i}>{part.bold}</b>
@@ -43,7 +43,7 @@ export default function SectionCta() {
             </div>
             <div className="h-[10svh] lg:h-0 "></div>
 
-            <div className="lg:h-[100svh] h-[35svh] w-4/5 lg:w-full bg-wlite flex flex-col justify-center md:justify-end py-[5rem]  ">
+            <div className="lg:h-[100svh] min-h-[35svh] w-4/5 lg:w-full bg-wlite flex flex-col justify-center md:justify-end py-[5rem]  ">
               <p className="text-diphblack text-xl md:text-2xl md:leading-[3rem] 2xl:text-3xl px-[2rem] lg:text-center 2xl:leading-[3rem] tracking-wider ">
                 {" "}
                 {home.ctaright.map((part, i) =>
--- components/sectionshome/SectionServices.tsx
+++ components/sectionshome/SectionServices.tsx
@@ -1,27 +1,16 @@
 "use client";
 import { useLanguage } from "@/components/language/LangContext";
-import { useEffect, useState } from "react";
 
 export default function SectionServices() {
   const { dictionary } = useLanguage();
-  const [speed, setSpeed] = useState("1.3");
 
   const services = dictionary.homepage.services;
-
-  useEffect(() => {
-    const updateSpeed = () => {
-      setSpeed(window.innerWidth < 900 ? "1" : "1.3");
-    };
-    updateSpeed(); // set once on mount
-    window.addEventListener("resize", updateSpeed);
-    return () => window.removeEventListener("resize", updateSpeed);
-  }, []);
 
   return (
     <div className="flex flex-col-reverse lg:flex-row lg:min-h-[220svh] font-urbanistr border-b border-wlite ">
       {/* ----------------------ARTISTS ---------------------- */}
       <div className="bg-wlite lg:w-1/2  text-diphblack py-20  lg:py-[50svh] px-5 md:px-10">
-        <div data-speed={speed} className="flex flex-col text-xl md:text-2xl">
+        <div className="flex flex-col text-xl md:text-2xl">
           <h3 className=" text-4xl md:text-5xl  lg:text-[5rem] 2xl:text-[6rem]  ">
             {" "}
             {services.artists.h3}{" "}
@@ -54,10 +43,7 @@ export default function SectionServices() {
         <h2 className="font-menlor text-[11svw] md:text-[10svw] lg:text-[4rem] 2xl:text-[6rem]  text-center">
           {services.h2}
         </h2>
-        <div
-          data-speed={speed}
-          className=" pt-20 lg:pt-[40svh] 2xl:pt-[70svh] px-5 md:px-10 "
-        >
+        <div className=" pt-20 lg:pt-[40svh] 2xl:pt-[70svh] px-5 md:px-10 ">
           <h3 className=" text-4xl md:text-5xl lg:text-[5rem] 2xl:text-[6rem]  ">
             {" "}
             {services.business.h3}{" "}
